7fe81981dd46a8ca10b0ffd4e68b76c77cbd0d8b
stresszero <restouri@naver.com>
2022-10-09 13:01:41 +0900

    fix: socketio disconnect

--- chat/socketio.py
+++ chat/socketio.py
@@ -19,32 +19,29 @@ sio = socketio.Server(
     # logger=True
 )
 
-users = {}
-
 
 @sio.event
 def connect(sid, environ, auth):
     """
     socket.io 클라이언트가 연결되면 아래 코드 실행
-    auth의 사용자id값으로 해당 사용자가 존재하지않거나 차단유저이면 connect_error로 emit
-    emit 되면 클라이언트 쪽에서 강제로 연결해제 처리
+    - 클라이언트가 보낸 auth의 사용자id값으로 해당 유저가 존재하지않거나 
+    차단된 유저이면 채팅방 입장 안되게 처리
     """
     user = User.objects.filter(id=auth["userId"])
     if not user.exists() or user.get().status == UserStatus.BANNED.value:
-        sio.emit("connect_error", {"message": "invalid user"})
+        return False
 
 
 @sio.on("join")
 def handle_join(sid, data):
     """
     유저가 채팅방에 입장하면 아래 코드 실행
-    브라우저에서 채팅방 입장시 connect 이벤트가 먼저 발생하고 이어서 join 이벤트가 발생됨
-    data를 자바스크립트 객체로 받으면 파이썬 딕셔너리로 사용 가능
-    data에는 방 번호와 사용자 닉네임이 포함됨
-    채팅방에 접속한 사용자의 sid값을 key로 하고, data를 value로 users 딕셔너리에 저장
+    - 브라우저에서 채팅방 입장시 connect 이벤트가 먼저 발생하고 이어서 join 이벤트가 발생됨
+    - data를 자바스크립트 객체로 받으면 파이썬 딕셔너리로 사용 가능
+    - data에는 유저 닉네임과 방 번호가 포함됨(key: nickname, room)
+    - save_session(sid, data): 채팅방에 접속한 클라이언트의 sid를 기준으로 data를 세션 데이터로 저장
     """
-    global users
-    users[sid] = data
+    sio.save_session(sid, data)
     sio.enter_room(sid, room=data["room"])
 
     sio.emit(
@@ -63,9 +60,9 @@ def handle_send_message(
 ):
     """
     send_message 이벤트로 받은 데이터를 MongoDB에 저장하고 add_message로 emit
-    censor_text(): 욕설 필터링 함수, 채팅 메시지에 욕설이 있으면 그 부분만 *으로 수정한 문자열을 반환함
-    save_message(): MongoDB에 메시지 데이터를 저장하고 고유한 ObjectId(24자리 문자열)반환
-    필요한 데이터 처리가 완료되면 데이터를 add_message로 emit
+    - censor_text(): 욕설 필터링 함수, 채팅 메시지에 욕설이 있으면 그 부분만 *으로 수정한 문자열을 반환함
+    - save_message(): MongoDB에 메시지 데이터를 저장하고 고유한 ObjectId(24자리 문자열)반환
+    - 필요한 메시지 처리가 완료되면 data를 add_message로 emit
     """
     data = {
         "user_nickname": nickname,
@@ -83,20 +80,18 @@ def handle_send_message(
 def disconnect(sid):
     """
     socket.io 클라이언트가 연결 해제되면 아래 코드 실행
-    leave_username: 연결 해제된 sid값의 유저 닉네임
-    leave_room_number: 연결 해제된 sid값의 방 번호
-    퇴장한 사용자 정보를 담아 add_message로 emit
-    users 딕셔너리에서 해당 sid값의 데이터 삭제
+    - get_session(sid): 위에서 save_session으로 저장한 sid별 세션 데이터 딕셔너리를 반환함
+    - disconnected_username: 연결 해제된 sid의 유저 닉네임
+    - room_number: 연결 해제된 sid가 있던 방 번호
+    - 퇴장한 사용자 정보를 담아 add_message로 emit
     """
-    global users
-    leave_username = users[sid].get("nickname")
-    leave_room_number = users[sid].get("room")
+    disconnected_username = sio.get_session(sid).get("nickname", "")
+    room_number = sio.get_session(sid).get("room")
     sio.emit(
         "add_message",
         {
             "user_nickname": "함께하개 관리자",
-            "text"         : f"{leave_username}님이 퇴장하셨어요."
+            "text"         : f"{disconnected_username}님이 퇴장하셨어요."
         },
-        to=leave_room_number,
+        to=room_number,
     )
-    del users[sid]

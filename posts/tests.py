ae07c9d3478d080aac495267ec04346b542749a4
stresszero <restouri@naver.com>
2022-10-31 14:28:41 +0900

    test: add posts app test cases

--- posts/tests.py
+++ posts/tests.py
@@ -137,7 +137,7 @@ class GetDeletedPostByAdmin(PostTest):
             ),
             HTTP_AUTHORIZATION=f"Bearer {self.admin_jwt}",
         )
-        result = {
+        results = {
             "id": self.test_deleted_post.id,
             "subject": self.test_deleted_post.subject,
             "content": self.test_deleted_post.content,
@@ -154,7 +154,7 @@ class GetDeletedPostByAdmin(PostTest):
             "delete_reason": "등록된 삭제사유 없음",
         }
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), result)
+        self.assertEqual(response.json(), results)
 
     def test_fail_405_get_deleted_post_by_admin(self):
         response = self.client.post(
@@ -243,3 +243,71 @@ class GetPostTest(PostTest):
 
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(), {"detail": "Unauthorized"})
+
+
+class ModifyPostTest(PostTest):
+    pass
+
+
+class GetPostByAdminTest(PostTest):
+    def test_success_get_post_by_admin(self):
+        response = self.client.get(
+            reverse(
+                "api-1.0.0:get_post_by_admin",
+                kwargs={"post_id": self.test_post.id},
+            ),
+            HTTP_AUTHORIZATION=f"Bearer {self.admin_jwt}",
+        )
+        results = {
+            "id": self.test_post.id,
+            "subject": self.test_post.subject,
+            "content": self.test_post.content,
+            "created_at": f"{self.test_post.created_at.isoformat()[:-9]}Z",
+            "image_url": self.test_post.image_url,
+            "user_id": self.test_post.user_id,
+            "user_name": self.test_post.user.name,
+            "user_nickname": self.test_post.user.nickname,
+            "user_email": self.test_post.user.email,
+            "user_mbti": self.test_post.user.mbti,
+            "user_address": self.test_post.user.address,
+            "user_thumbnail": self.test_post.user.thumbnail_url,
+            "user_created_at": f"{self.test_post.user.created_at.isoformat()[:-9]}Z",
+            "comments_list": [
+                {
+                    "id": comment.id,
+                    "created_at": f"{comment.created_at.isoformat()[:-9]}Z",
+                    "content": comment.content,
+                    "user_id": comment.user_id,
+                    "post_id": comment.post_id,
+                    "user_nickname": comment.user.nickname,
+                    "user_thumbnail": comment.user.thumbnail_url,
+                }
+                for comment in self.test_post.comments.filter(
+                    is_deleted=False
+                ).order_by("created_at")
+            ],
+        }
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), results)
+
+    def test_fail_405_get_post_by_admin(self):
+        response = self.client.post(
+            reverse(
+                "api-1.0.0:get_post_by_admin",
+                kwargs={"post_id": self.test_post.id},
+            ),
+            HTTP_AUTHORIZATION=f"Bearer {self.admin_jwt}",
+        )
+        self.assertEqual(response.status_code, 405)
+        self.assertContains(response, "Method not allowed", status_code=405)
+
+    def test_fail_403_get_post_by_admin(self):
+        response = self.client.get(
+            reverse(
+                "api-1.0.0:get_post_by_admin",
+                kwargs={"post_id": self.test_post.id},
+            ),
+            HTTP_AUTHORIZATION=f"Bearer {self.user_jwt}",
+        )
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response.json(), {"detail": "forbidden"})
